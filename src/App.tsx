5a0796e2cc6f923c9bca9736f262393ee21b2a77
faxio <the_fabio@live.cl>
2023-10-03 14:45:04 -0300

    add fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,61 @@
 import "./App.css";
+import styled from "styled-components";
+import { useState, useEffect } from "react";
 const url = import.meta.env.VITE_URL_ENDPOINTS;
 
+const Header = styled.header`
+  background-color: #fbfbfe;
+  margin: 0;
+  padding: 1rem;
+  border-bottom: 1px solid black;
+`;
+
+const Title = styled.h1`
+  color: #050315;
+  font-size: 2rem;
+  font-weight: 400;
+`;
+
+interface Interaction {
+  command: string;
+  message: string;
+}
+
 function App() {
-  console.log(url);
+  const [interactions, setInteractions] = useState<Interaction[]>([]);
+
+  useEffect(() => {
+    fetch(`${url}/api/command`)
+      .then((response) => response.json())
+      .then(({ data }) => setInteractions(data));
+  }, []);
 
   return (
     <>
       {/* HEADER */}
-      <h1>Bot APP</h1>
+      <Header>
+        <Title>Bot APP</Title>
+        <button>Reiniciar</button>
+        <button>Iniciar</button>
+        <button>Parar</button>
+      </Header>
 
-      {/* Search bar */}
+      <article>
+        {/* Search bar */}
 
-      {/* Create interaction*/}
+        <input placeholder="Search"></input>
 
-      {/* Interactions */}
+        {/* Create interaction*/}
+        <button> Create Interaction</button>
+
+        {/* Interactions */}
+        {interactions.map((interaction: Interaction, Index) => (
+          <div key={Index}>
+            <p>{interaction.command}</p>
+            <p>{interaction.message}</p>
+          </div>
+        ))}
+      </article>
     </>
   );
 }
